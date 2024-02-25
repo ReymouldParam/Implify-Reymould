136f6ce006e71c1a77175d6efda57044fe0b54ff
Revanth_Reymould <revanth.danduboina@reymould.com>
2024-02-26 00:17:20 +0530

    Case study page added

--- assets/js/global.js
+++ assets/js/global.js
@@ -16,7 +16,7 @@ const observeSlideUp = new IntersectionObserver((entries) => {
   const slideUpSections = document.querySelectorAll(".scrollAnimation");
   slideUpSections.forEach((ele) => observeSlideUp.observe(ele));
 
-  // Animation 1 scroll fixed animations
+  // Animation 2 scroll fixed animations
 const observeSlideUp2 = new IntersectionObserver((entries) => {
     entries.forEach((entry)=> {
       if(entry.isIntersecting){
@@ -27,3 +27,17 @@ const observeSlideUp2 = new IntersectionObserver((entries) => {
   
   const slideUpSections2 = document.querySelectorAll(".scrollAnimationFixed");
   slideUpSections2.forEach((ele) => observeSlideUp2.observe(ele));
+
+    // Animation 3 width expand animations
+const observeSlideUp3 = new IntersectionObserver((entries) => {
+  entries.forEach((entry)=> {
+    if(entry.isIntersecting){
+      entry.target.classList.add('defaultWidth');
+    }else{
+      entry.target.classList.remove('defaultWidth');
+    }
+  });
+});
+
+const slideUpSections3 = document.querySelectorAll(".widthAnimation");
+slideUpSections3.forEach((ele) => observeSlideUp3.observe(ele));
